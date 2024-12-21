f2f547783af03b47b25f7959582cea5f93fbee2c
nguyenphong1996 <phongndbps35129@fpt.edu.vn>
2024-12-22 01:07:22 +0700

    video 30, fagment, sytpe inline

--- src/components/learn/TestComponennt.jsx
+++ src/components/learn/TestComponennt.jsx
@@ -1,32 +1,32 @@
 const SecondComponennt = () => {
     return (
-        <div>
-            <p>Lorem ipsum dolor sit amet consectetur, \n
+        <>
+            <p>Lorem ipsum dolor sit amet consectetur,
                 adipisicing elit. Doloribus neque ipsa a ab  \n reprehenderit expedita numquam nobis maiores culpa optio dolores
                 dolorem aliquam rerum ad alias aliquid, sapiente reiciendis magni?</p>
-        </div>
+        </>
     );
 }
 
 const ThirdComponennt = () => {
     return (
-        <div>
+        <>
             <header>
                 Đây là header
             </header>
-            {/* <body style={"display: flex"} >
-                <aside style={"flex: 3"}>
+            <div style={{ display: "flex" }} >
+                <div style={{ flex: 3 }}>
                     <p> Đây là nội dung bên trái</p>
-                </aside >
-                <article style={"flex: 7"}>
+                </div >
+                <div style={{ flex: 7 }}>
                     <p>Đây là nội dung bên phải</p>
-                </article>
-            </body> */}
-            <footer>
+                </div>
+            </div>
+            <div>
                 Đây là chân trang
-            </footer>
+            </div>
 
-        </div>
+        </>
     )
 }
 
