956e89481637d3ce809f239e4ce6f6d734f0129f
nguyenphong1996 <phongndbps35129@fpt.edu.vn>
2024-12-27 00:31:13 +0700

    video 34

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-
 import './components/source/Todo.css'
 import TodoInput from './components/source/TodoInput'
 import TodoList from './components/source/TodoList'
 
 function App() {
+  const myname = 'phongnguyen';
+  const age = 18;
+  const data = {
+    address: 'TPHCM',
+    phone: '0123456789',
+    email: 'phongnguyen.pha@gmail.com'
+  }
   return (
     <div className='container'>
       <p className='titleText'>Todo list</p>
       <TodoInput />
-      <TodoList />
+      <TodoList myname={myname} age={age} data={data}  />
     </div>
   )
 }
--- src/components/source/TodoList.jsx
+++ src/components/source/TodoList.jsx
@@ -1,10 +1,17 @@
 
-const TodoList = () => {
+const TodoList = (props) => {
+    const { myname , age, data} = props
+    // console.log('checkprops', dataList)
     return (
         <div className="containerTodoList">
             <p className="Todo">Learn ReactJS</p>
             <p className="Todo">Learn React Native</p>
             <p className="Todo">Learn Docker</p>
+            <p>my name is: {myname}</p>
+            <p>I'm {age} year old</p>
+            <p>My address: {data.address}</p>
+            <p>My phone: {data.phone}</p>
+            <p>My email: {data.email}</p>
         </div>
     );
 }
